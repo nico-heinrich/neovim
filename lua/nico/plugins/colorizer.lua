940f248448d65dfd6eb41c8daece6bb7a841f8bb
Nico Heinrich <heinrich.nico@gmail.com>
2023-11-06 23:40:46 +0100

    added colorizer

---
+++ lua/nico/plugins/colorizer.lua
@@ -0,0 +1,7 @@
+return {
+  "NvChad/nvim-colorizer.lua",
+  opt = true,
+  config = function()
+    require("colorizer").setup()
+  end
+}
--- lua/nico/plugins/lualine.lua
+++ lua/nico/plugins/lualine.lua
@@ -3,8 +3,8 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
     opt = true,
-    config = function()
-      require("lualine").setup()
-    end
-  }
+  },
+  config = function()
+    require("lualine").setup()
+  end
 }
