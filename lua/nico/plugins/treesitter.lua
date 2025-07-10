c9922659a16abf45a532bdce319ccd696340a4f6
DevExord <dev@exord.de>
2025-07-10 23:35:21 +0200

    install treesitter ts

--- lua/nico/plugins/treesitter.lua
+++ lua/nico/plugins/treesitter.lua
@@ -1,32 +1,32 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function () 
-    local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-        context_commentstring = {
-          enable = true,
-          enable_autocmd = false,
-        },
-        ensure_installed = {
-          "c",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-          "svelte",
-          "css",
-          "javascript",
-          "html",
-          "glimmer",
-        },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },  
-      })
+		configs.setup({
+			context_commentstring = {
+				enable = true,
+				enable_autocmd = false,
+			},
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"svelte",
+				"css",
+				"javascript",
+				"typescript",
+				"html",
+				"glimmer",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
 
-      vim.cmd("autocmd BufNewFile,BufRead *.handlebars set filetype=glimmer") -- see .handlebars as glimmer files
-  end
+		vim.cmd("autocmd BufNewFile,BufRead *.handlebars set filetype=glimmer") -- see .handlebars as glimmer files
+	end,
 }
-
